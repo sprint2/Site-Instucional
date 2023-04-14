372e58eb0b0b45d8774d9a98c91732b10923540c
zzzmiike <michaelhenrique0022@gmail.com>
2023-04-14 13:23:47 -0300

    Subindo alterações, cadastro com integração ao banco de Dados

--- public/script/script.js
+++ public/script/script.js
@@ -1,3 +1,60 @@
+// sessão
+function validarSessao() {
+    // aguardar();
+
+    var email = sessionStorage.EMAIL_USUARIO;
+    var nome = sessionStorage.NOME_USUARIO;
+
+    var b_usuario = document.getElementById("b_usuario");
+    var b_email = document.getElementById("b_email");
+
+    if (email != null && nome != null) {
+        // window.alert(`Seja bem-vindo, ${nome}!`);
+        b_usuario.innerHTML = nome;
+        b_email.innerHTML = email;
+
+        // finalizarAguardar();
+    } else {
+        window.location = "../login.html";
+    }
+}
+
+function limparSessao() {
+    // aguardar();
+    sessionStorage.clear();
+    // finalizarAguardar();
+    window.location = "../login.html";
+}
+
+// carregamento (loading)
+function aguardar() {
+    var divAguardar = document.getElementById("div_aguardar");
+    divAguardar.style.display = "flex";
+}
+
+function finalizarAguardar(texto) {
+    var divAguardar = document.getElementById("div_aguardar");
+    divAguardar.style.display = "none";
+
+    var divErrosLogin = document.getElementById("div_erros_login");
+    if (texto) {
+        divErrosLogin.style.display = "flex";
+        divErrosLogin.innerHTML = texto;
+    }
+}
+
+
+// modal
+function mostrarModal() {
+    var divModal = document.getElementById("div_modal");
+    divModal.style.display = "flex";
+}
+
+function fecharModal() {
+    var divModal = document.getElementById("div_modal");
+    divModal.style.display = "none";
+}
+
 function simular() {
     litros = Number(document.getElementById('inp_litros').value)
     vendaLitro = Number(document.getElementById('inp_venda_litro').value)
@@ -44,7 +101,7 @@ function simular() {
                     #resultado {
                         display: flex;
                         align-items: center;
-                        width: 55%;
+                        width: 60%;
                         background-color: #FFFFFF;
                         font-family: 'Inter';
                         font-style: normal;
