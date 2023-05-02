f572b1b5f6474a32ac4189ff9506977513815be9
zzzmike <michaelhenrique0022@gmail.com>
2023-05-01 23:06:39 -0300

    Atualizaçâo dos Diretorios com os Arquivos corretos

--- public/script/script.js
+++ public/script/script.js
@@ -1,3 +1,5 @@
+
+// animação da navbar
 window.onscroll = function() {
     var nav = document.getElementById("navbar");
     var navLink = document.getElementsByClassName("nav-link");
@@ -74,22 +76,22 @@ function fecharModal() {
 }
 
 function simular() {
-    litros = Number(document.getElementById('inp_litros').value)
-    vendaLitro = Number(document.getElementById('inp_venda_litro').value)
-    custoProd = Number(document.getElementById('inp_custo_prod').value)
+    litros = Number(document.getElementById('inp_litros').value);
+    vendaLitro = Number(document.getElementById('inp_venda_litro').value);
+    custoProd = Number(document.getElementById('inp_custo_prod').value);
 
 
-    let litrosCPerda = litros - (litros * 0.1)
-    let vendaFinal = vendaLitro - custoProd
-    let ganhoMes = vendaFinal * litrosCPerda
-    let ganhoAno = ganhoMes * 12
-    let perda = ((litros - litrosCPerda) * vendaFinal) * 12
+    let litrosCPerda = litros - (litros * 0.1);
+    let vendaFinal = vendaLitro - custoProd;
+    let ganhoMes = vendaFinal * litrosCPerda;
+    let ganhoAno = ganhoMes * 12;
+    let perda = ((litros - litrosCPerda) * vendaFinal) * 12;
 
-    let litrosCSolucao = litros - (litros * 0.02)
-    let vendaFinalSolucao = vendaLitro - custoProd
-    let ganhoMesSolucao = vendaFinal * litrosCSolucao
-    let ganhoAnoSolucao = ganhoMesSolucao * 12
-    let perdaSolucao = ((litros - litrosCSolucao) * vendaFinal) * 12
+    let litrosCSolucao = litros - (litros * 0.02);
+    let vendaFinalSolucao = vendaLitro - custoProd;
+    let ganhoMesSolucao = vendaFinal * litrosCSolucao;
+    let ganhoAnoSolucao = ganhoMesSolucao * 12;
+    let perdaSolucao = ((litros - litrosCSolucao) * vendaFinal) * 12;
 
 
     resultado.innerHTML = `
@@ -114,4 +116,183 @@ function simular() {
             </tr>
         </table>
     `
+};
+
+function trocarPaginaDash() {
+    var url = window.location.href;
+
+    if(url.endsWith('dash-geral.html') == true) {
+        window.location.href = '../armazem/dash-armazem.html';
+    }  else {
+        window.location.href = '../geral/dash-geral.html';
+    };
+};
+
+window.onload = function() {
+    const data = new Date();
+
+    var dia = String(data.getDate()).padStart(2, '0');
+    var mes = String(data.getMonth()+1).padStart(2,"0");
+    var ano = data.getFullYear();
+
+    document.getElementById("span_data").innerHTML = `${dia}/${mes}/${ano}`;
+};
+
+function entrar() {
+    aguardar();
+
+    var emailVar = inp_email.value;
+    var senhaVar = inp_senha.value;
+
+    if (emailVar == "" || senhaVar == "") {
+        cardErro.style.display = "block"
+        mensagem_erro.innerHTML = "Preencha todos os campos";
+        inp_email.style.borderColor = "#b03838";
+        inp_senha.style.borderColor = "#b03838";
+        finalizarAguardar();
+        return false;
+    }
+    else {
+        setInterval(sumirMensagem, 5000)
+    }
+
+    console.log("FORM LOGIN: ", emailVar);
+    console.log("FORM SENHA: ", senhaVar);
+
+    fetch("/empresa/autenticar", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            emailServer: emailVar,
+            senhaServer: senhaVar
+        })
+    }).then(function (resposta) {
+        console.log("ESTOU NO THEN DO entrar()!")
+
+        if (resposta.ok) {
+            console.log(resposta);
+
+            resposta.json().then(json => {
+                console.log(json);
+                console.log(JSON.stringify(json));
+
+                sessionStorage.EMAIL_EMPRESA = json.email;
+                sessionStorage.NOME_EMPRESA = json.nome;
+                sessionStorage.ID_EMPRESA = json.id;
+
+                setTimeout(function () {
+                    window.location = "./dashboard/geral/dash-geral.html";
+                }, 1000); // apenas para exibir o loading
+
+            });
+
+        } else {
+
+            console.log("Houve um erro ao tentar realizar o login!");
+
+            resposta.text().then(texto => {
+                console.error(texto);
+                finalizarAguardar(texto);
+            });
+        }
+
+    }).catch(function (erro) {
+        console.log(erro);
+    })
+
+    return false;
+}
+
+function sumirMensagem() {
+    cardErro.style.display = "none"
+}
+
+function cadastrar() {
+    //aguardar();
+    //Recupere o valor da nova input pelo nome do id
+    // Agora vá para o método fetch logo abaixo
+    var nomeVar = inp_nome.value;
+    var cnpjVar = inp_cnpj.value.replace(/\D+/g, '');
+    var telVar = inp_telefone.value.replace(/\D+/g, '');
+    var emailVar = inp_email.value;
+    var senhaVar = inp_senha.value;
+    var confiSenha = inp_confiSenha.value;
+
+    if (confiSenha == senhaVar) {
+        if (nomeVar == "" || cnpjVar == "" || telVar == "" || emailVar == "" || senhaVar == "" || confiSenha == "") {
+            cardErro.style.opacity = "1"
+            mensagem_erro.innerHTML = "Preencha todos os campos";
+            inp_senha.style.borderColor = "#b03838";
+            inp_confiSenha.style.borderColor = "#b03838";
+            inp_email.style.borderColor = "#b03838";
+            inp_telefone.style.borderColor = "#b03838";
+            inp_cnpj.style.borderColor = "#b03838";
+            inp_nome.style.borderColor = "#b03838";
+            finalizarAguardar();
+            return false;
+        } else if (cnpjVar.length == 14) {
+            // Enviando o valor da nova input
+            fetch("empresa/cadastrar", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    // crie um atributo que recebe o valor recuperado aqui
+                    // Agora vá para o arquivo routes/empresa.js
+                    nomeServer: nomeVar,
+                    cnpjServer: cnpjVar,
+                    telServer: telVar,
+                    emailServer: emailVar,
+                    senhaServer: senhaVar
+                })
+            }).then(function (resposta) {
+
+                console.log("resposta: ", resposta);
+
+                if (resposta.ok) {
+                    cardErro.style.opacity = "1";
+
+                    modal.style.top = '0'
+
+                    setTimeout(() => {
+                        window.location = "login.html";
+                    }, "2500")
+
+                    limparFormulario();
+                    finalizarAguardar();
+                } else {
+                    throw ("Houve um erro ao tentar realizar o cadastro!");
+                }
+            }).catch(function (resposta) {
+                console.log(`#ERRO: ${resposta}`);
+                finalizarAguardar();
+            });
+
+            return false;
+        } else {
+            cardErro.style.opacity = "1"
+            mensagem_erro.innerHTML = "O CNPJ está incorreto";
+            inp_cnpj.style.borderColor = "#dd0000"
+
+            finalizarAguardar();
+            return false;
+            setInterval(sumirMensagem, 5000)
+        }
+
+
+    } else {
+        cardErro.style.opacity = "1"
+        mensagem_erro.innerHTML = "As senhas não correspondem";
+        inp_senha.style.borderColor = "#dd0000";
+        inp_confiSenha.style.borderColor = "#dd0000"
+
+        finalizarAguardar();
+        return false;
+    }
+}
+function sumirMensagem() {
+    cardErro.style.opacity = "0"
 }
