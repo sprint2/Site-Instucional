411bd88c9924fb14e756b6ee5c533af9e0a99274
zzzmiike <michaelhenrique0022@gmail.com>
2023-06-02 09:31:37 -0300

    Arrumando graficos

--- public/dashboard/script/dashboards.js
+++ public/dashboard/script/dashboards.js
@@ -348,7 +348,7 @@ function puxarArmazemUmid(idEmpresa) {
    new Chart(ctxUmdd, {
       type: "line",
       data: {
-         labels: ["Umidade"],
+         labels: [armazensUmid],
          datasets: [
             {
                label: "Níveis de umidade",
